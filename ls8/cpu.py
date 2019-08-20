f3748c5bd024386a0b0b0b18c8f36ca9b53213cd
Divya Nair <divnairs@gmail.com>
2019-08-19 17:59:35 -0700

    finished run(): HLT, PRN, LDI

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -5,6 +5,8 @@ class CPU:
     # Construct a new CPU
     def __init__(self):
         self.ram = [0] * 256
+        self.reg = [0] * 8
+        self.pc = 0
 
 
     # Load a program into memory
@@ -66,4 +68,27 @@ class CPU:
 
     # Run the CPU
     def run(self):
-        pass
+        LDI = 0b10000010
+        PRN = 0b01000111
+        HLT = 0b00000001
+
+        running = True
+
+        while running:
+            ir = self.ram_read(self.pc)
+
+            # print("IR: {}, HLT: {}, PRN: {}, LDI: {}, opA: {}, opB: {}".format(ir, HLT, PRN, LDI,self.ram_read(self.pc+1), self.ram_read(self.pc+2)))
+
+            if ir == HLT:
+                running = False
+
+            if ir == PRN:
+                reg = self.ram_read(self.pc + 1)
+                print(self.reg[reg])
+                self.pc += 2
+
+            if ir == LDI:
+                reg_a = self.ram_read(self.pc + 1)
+                reg_b = self.ram_read(self.pc + 2)
+                self.reg[reg_a] = reg_b
+                self.pc += 3       
